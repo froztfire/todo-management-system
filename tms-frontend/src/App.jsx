21c71206bd0a59e36070f41a4afa1318fd560481
2194063 <jeromegarcia.16@gmail.com>
2024-04-12 14:11:02 +0800

    Adding Header and Footer

--- tms-frontend/src/App.jsx
+++ tms-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import FooterComponents from "./components/FooterComponents";
+import HeaderComponent from "./components/HeaderComponent";
 import ListTodoComponent from "./components/ListTodoComponent";
 
 function App() {
   return (
     <>
-      <ListTodoComponent />;
+      <HeaderComponent />
+      <ListTodoComponent />
+      <FooterComponents />
     </>
   );
 }
---
+++ tms-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="https://www.javaguides.net">
+            Todo Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
