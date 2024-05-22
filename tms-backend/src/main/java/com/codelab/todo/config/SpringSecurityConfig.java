6eb2bfb306d819ea1eb43db073cc2afa9f8ab8c4
2194063 <jeromegarcia.16@gmail.com>
2024-05-22 21:23:42 +0800

    Configure Role-Based Authorization

--- tms-backend/src/main/java/com/codelab/todo/config/SpringSecurityConfig.java
+++ tms-backend/src/main/java/com/codelab/todo/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.codelab.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -24,6 +25,11 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+            authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+            authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
             authorize.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
         return http.build();
