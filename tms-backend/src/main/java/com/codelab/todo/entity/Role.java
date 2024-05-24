78da762e5858c2110bddd9f856f11d42f77c1d57
2194063 <jeromegarcia.16@gmail.com>
2024-05-24 19:40:56 +0800

    Create Users and Roles Repository

--- tms-backend/src/main/java/com/codelab/todo/entity/Role.java
+++ tms-backend/src/main/java/com/codelab/todo/entity/Role.java
@@ -15,6 +15,6 @@ import lombok.Setter;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 }
